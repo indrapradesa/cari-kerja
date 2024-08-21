72bc7efd534fcec2c385bc046f3040233f2e4574
indrapradesa <indrapradesa77@gmail.com>
2024-08-22 02:19:45 +0700

    dd

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,13 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/toggleJobUpdate.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+                'resources/js/toggleJobUpdate.js',
+                'resources/js/partnerJobUpdate.js', // tambahkan .js di sini
+            ],
             refresh: true,
         }),
     ],
-    build: {
-        rollupOptions: {
-            input: {
-                main: 'resources/js/toggleJobUpdate.js',
-            },
-        },
-    },
 });
